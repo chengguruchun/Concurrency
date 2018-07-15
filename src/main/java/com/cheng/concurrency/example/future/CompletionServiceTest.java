872a9ecdd78ea89e0abb36cfeb2ea9b06efc479b
chengguruchun <1725788349@qq.com>
2018-07-15 13:39:59 +0800

    future理解，，aqs实现

---
+++ src/main/java/com/cheng/concurrency/example/future/CompletionServiceTest.java
@@ -0,0 +1,61 @@
+package com.cheng.concurrency.example.future;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * @Author :cheng
+ * @Description:
+ * @Date: created in 13:30 2018/7/15
+ * @Reference:
+ */
+public class CompletionServiceTest {
+    public static void main(String[] args) {
+        try {
+            completionServiceCount();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 使用completionService收集callable结果
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    public static void completionServiceCount() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        CompletionService<Integer> completionService = new ExecutorCompletionService<Integer>(executorService);
+        int threadNum = 5;
+        for (int i = 0; i < threadNum; i++) {
+            completionService.submit(getTask(i));
+        }
+        int sum = 0;
+        int tem = 0;
+        for (int i = 0; i < threadNum; i++) {
+            tem = completionService.take().get();
+            sum += tem;
+            System.out.println(tem + "\t");
+        }
+
+        System.out.println("CompletionService all is : " + sum);
+        executorService.shutdown();
+    }
+
+    public static Callable<Integer> getTask(final int no) {
+        final Random rand = new Random();
+        Callable<Integer> task = new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                int time = rand.nextInt(100)*100;
+                System.out.println("thead:"+no+" time is:"+time);
+                Thread.sleep(time);
+                return no;
+            }
+        };
+        return task;
+    }
+}
